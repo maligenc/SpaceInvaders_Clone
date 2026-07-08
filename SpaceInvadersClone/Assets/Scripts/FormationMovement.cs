39b98e595ad72ae3d7a623b2108755e544a1e2ca
maligenc <maligenc01@gmail.com>
2026-07-09 00:37:22 +0300

    FormationMovement without collider movement calculated by child position

--- SpaceInvadersClone/Assets/Scripts/FormationMovement.cs
+++ SpaceInvadersClone/Assets/Scripts/FormationMovement.cs
@@ -6,21 +6,50 @@ public class FormationMovement : MonoBehaviour
 
     [SerializeField]
     private float speed = 10f;
-    public Vector2 direction = Vector2.right;
+
+    [SerializeField]
+    private Transform FormationCenter;
+
+    private Vector2 direction = Vector2.right;
+
+    public Vector2 farRightChild = Vector2.zero;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    void OnCollisionEnter2D()
+
+    void Update()
     {
-        speed *=-1.0f;
+        foreach (Transform child in transform)
+        {
+            if (speed > 0)
+            {
+                if(child.position.x > farRightChild.x)
+                {
+                    farRightChild.x=child.position.x;
+                }
+            }
+            else
+            {
+                if(child.position.x < farRightChild.x)
+                {
+                    farRightChild.x=child.position.x;
+                }
+            }
+        }
     }
 
     void FixedUpdate()
     {
+        if (farRightChild.x < -7 ||farRightChild.x>7)
+        {
+            speed *=-1;
+        }
         Vector2 Movement = direction *speed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position+Movement);
+
     }
 
 }
