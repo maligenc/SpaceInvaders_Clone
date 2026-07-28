9f703cf79465e9a47ac514bfa514fdbd7c67aeb7
maligenc <maligenc01@gmail.com>
2026-07-29 00:30:12 +0300

    failed entry animation attempt

--- SpaceInvadersClone/Assets/Scripts/Enemy/GetInScene.cs
+++ SpaceInvadersClone/Assets/Scripts/Enemy/GetInScene.cs
@@ -1,21 +1,26 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class GetingInScene : MonoBehaviour
+public class GetInScene : MonoBehaviour
 {
     [SerializeField] private float initalSpeed = -2f;
     [SerializeField] private Rigidbody2D enemySpaceShip;
+    [SerializeField] private AnimationCurve curve;
+    [SerializeField] private float duration = 0f;
 
 
     void Start()
     {
         enemySpaceShip = GetComponent<Rigidbody2D>();
         enemySpaceShip.linearVelocityY= initalSpeed;
+
     }
 
 
     void Update()
     {
-        enemySpaceShip.linearVelocityY += 4f;
+        float currentTime =0f;
+        enemySpaceShip.linearVelocityY = enemySpaceShip.linearVelocityY * curve.Evaluate(currentTime/duration);
     }
+
 }
