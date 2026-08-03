cbe9f7193f6c0a58f0361af5f4d9852c54737fc8
maligenc <maligenc01@gmail.com>
2026-08-03 19:37:07 +0300

    unnecessary code removed

--- SpaceInvadersClone/Assets/Scripts/UI/ScoreController.cs
+++ SpaceInvadersClone/Assets/Scripts/UI/ScoreController.cs
@@ -8,21 +8,17 @@ public class ScoreController : MonoBehaviour
     [SerializeField] private int killPoint=0;
     private TMP_Text Score;
     public Transform formationTransfrom;
-    private int initialEnemyCount=0;
-    public float scoreMultiplier = 1.0f;
+
 
 
     void Start()
     {
         Score = GetComponent<TMP_Text>();
-        initialEnemyCount=formationTransfrom.childCount;
         scoreManager = GameObject.Find("GameManager").GetComponent<ScoreManager>();
     }
 
     void Update()
     {
-        // int killedEnemyCount = initialEnemyCount-formationTransfrom.childCount;
-        // int score = killedEnemyCount*killPoint;
         float totalScore = scoreManager.totalScore;
         Score.text = $"Score:{totalScore}";    
     }
